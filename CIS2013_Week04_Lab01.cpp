8310d4ae97e31688cf322987c57837e2171382bd
Natasha S Love <debora@inbox.ru>
2018-02-07 19:42:16 -0600

    lab

--- CIS2013_Week04_Lab01.cpp
+++ CIS2013_Week04_Lab01.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 using namespace std;
+// function calculates the result every time separately from previous calculations;
+int num=0;
 
+// void returns smth
+
+void print(int num) {
+	cout << "printing " << (num * 5) << endl;
+}
+
+// the loop counts how many times to repeat and gives a new number for num every time.
 int main (){
 
-
-
-
-
-
+for(int i=0; i<10; i++) {
+	print (i);
+}
+	
+// last number we want to see calculated
+print (1000);
 
 return 0;
 
