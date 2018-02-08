fd295785b0444a1ffcae8542b55ad5fabcfb8de7
Natasha S Love <debora@inbox.ru>
2018-02-07 19:47:04 -0600

    the better way

--- CIS2013_Week04_Lab01.cpp
+++ CIS2013_Week04_Lab01.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 // function calculates the result every time separately from previous calculations;
-int num=0;
 
 // void returns smth
 
-void print(int num) {
-	cout << "printing " << (num * 5) << endl;
-}
+void print(int) ;
 
 // the loop counts how many times to repeat and gives a new number for num every time.
 int main (){
@@ -21,4 +18,7 @@ print (1000);
 
 return 0;
 
+}
+void print(int num) {
+	cout << "printing " << (num * 5) << endl;
 }
