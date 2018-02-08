7cef9259fbc8d02b098b33c4302240b8213585c0
Natasha S Love <debora@inbox.ru>
2018-02-07 20:16:38 -0600

    functions

--- CIS2013_Week04_Lab01.cpp
+++ CIS2013_Week04_Lab01.cpp
@@ -4,21 +4,32 @@ using namespace std;
 
 // void returns smth
 
+int large_number = 0;
 void print(int) ;
+void print (int, int);
 
-// the loop counts how many times to repeat and gives a new number for num every time.
+// the loop counts how many times to repeat and gives a new number every time.
 int main (){
 
-for(int i=0; i<10; i++) {
-	print (i);
-}
+	for(int i=0; i<10; i++) {
+	
+		large_number=large_number+i;
+		// print(i, large_number);
+		cout << "Printing with one argument" << endl;
+		print(i);
+	}
 	
 // last number we want to see calculated
-print (1000);
+// print (large_number);
 
 return 0;
 
 }
 void print(int num) {
-	cout << "printing " << (num * 5) << endl;
+	cout << "printing x5 " << (num * 5) << " and total no total was given" << endl;
+} 
+
+void print(int num, int total){
+	cout << "printing x5 " << (num * 5) << " and total " << total << endl;
+	
 }
